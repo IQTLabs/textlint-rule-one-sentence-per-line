a6a66698c4c773f59b34e9ac32a11bbdb9dcb0ab
Benjamin Lee <benjamindlee@me.com>
2019-12-20 00:47:18 -0700

    Working prototype

--- src/index.js
+++ src/index.js
@@ -1,37 +1,92 @@
-// TODO: more reasonable example
+const splitter = require("sentence-splitter")
+
+/**
+ * Get parents of node.
+ * The parent nodes are returned in order from the closest parent to the outer ones.
+ * @param node
+ * @returns {Array}
+ */
+function getParents(node) {
+  const result = []
+  // child node has `parent` property.
+  let parent = node.parent
+  while (parent != null) {
+    result.push(parent)
+    parent = parent.parent
+  }
+  return result
+}
+
+/**
+ * Return true if `node` is wrapped any one of `types`.
+ * @param {TxtNode} node is target node
+ * @param {string[]} types are wrapped target node
+ * @returns {boolean|*}
+ */
+function isNodeWrapped(node, types) {
+  const parents = getParents(node)
+  const parentsTypes = parents.map(function(parent) {
+    return parent.type
+  })
+  return types.some(function(type) {
+    return parentsTypes.some(function(parentType) {
+      return parentType === type
+    })
+  })
+}
+
 const reporter = context => {
-  // `context.fixer`
   const { Syntax, RuleError, fixer, report, getSource } = context
   return {
     [Syntax.Str](node) {
-      const text = getSource(node)
-      // "You fix this"
-      //      ^^^
-      const matchRegexp = /\bfix\b/
-      if (!matchRegexp.test(text)) {
+      // Titles aren't sembr since breaks do affect them
+      if (isNodeWrapped(node, [Syntax.Header])) {
         return
       }
-      // found "fixable" error
-      const index = text.search(matchRegexp)
-      const length = "fix".length
-      const replace = fixer.replaceTextRange([index, index + length], "fixed")
+
+      const text = getSource(node)
+      const split = splitter.split(text)
+
+      // if there is only one sentence in the line, return early it's following sembr
+      if (split.filter(x => x.type == "Sentence").length == 1) {
+        return
+      }
+      // Sometimes, we might get a case where two lines separated by whitespaces are
+      // treated as a string Syntax.Str
+      //
+      // For example:
+      //
+      // Hello World!
+      // My Name is Benjamin.
+      //
+      // Would yield ["Hello World!", "\n", "My name is Benjamin."] when split by sentence-splitter
+      // This has two sentences in it but is proper sembr
+      else if (
+        split
+          .filter(x => x.type == "WhiteSpace")
+          .every(x => x.type == "WhiteSpace" && x.raw == "\n")
+      ) {
+        return
+      }
+
+      // insert the newlines
+      const fixed = split
+        .filter(x => x.type == "Sentence")
+        .map(x => x.raw)
+        .join("\n")
+
+      // when we found more than one sentence per line, propose a fix
       report(
         node,
-        new RuleError("Replaced.", {
-          // "You fix this"
-          //      ^ index
-          index,
-          // "You fix this"
-          //      ^^^
-          //     fixed
-          fix: replace
+        new RuleError("More than one sentence per line", {
+          fix: fixer.replaceText(node, fixed)
         })
       )
     }
   }
 }
+
 export default {
   linter: reporter,
-  // This rule has fixer.
   fixer: reporter
 }
--- test/index-test.js
+++ test/index-test.js
@@ -7,7 +7,13 @@ const rule = require("../src/index").default
 tester.run("rule", rule, {
   valid: [
     // no problem
-    "text"
+    "This is a sentence.\nThis is another sentence on a second line.",
+    "This is a sentence.\n> This is another sentence in a quotation.",
+    // Don't worry about multiple lines in titles
+    "# This is the title.\nThis is a single sentence.",
+    "# This is the title. It has two sentences.",
+    // or H2 titles
+    "## This is a second title. It has two sentences."
   ],
   invalid: [
     // single match
@@ -16,27 +22,57 @@ tester.run("rule", rule, {
       output: "This is a sentence.\nThis is another sentence.",
       errors: [
         {
-          message: "Replaced."
+          message: "More than one sentence per line"
+        }
+      ]
+    },
+    {
+      text:
+        "This is a sentence. This is another sentence. This is a third sentence.",
+      output:
+        "This is a sentence.\nThis is another sentence.\nThis is a third sentence.",
+      errors: [
+        {
+          message: "More than one sentence per line"
+        }
+      ]
+    },
+    // inside a quote
+    {
+      text:
+        "This is a sentence.\n> This is a quoted sentence. This is another sentence, which is also quoted.",
+      output:
+        "This is a sentence.\n> This is a quoted sentence.\nThis is another sentence, which is also quoted.",
+      errors: [
+        {
+          message: "More than one sentence per line"
+        }
+      ]
+    },
+
+    // inside a link
+    {
+      text:
+        "[This link has more than one sentence. It has two lines.](https://example.com)",
+      output:
+        "[This link has more than one sentence.\nIt has two lines.](https://example.com)",
+      errors: [
+        {
+          message: "More than one sentence per line"
+        }
+      ]
+    },
+
+    // inside emphasis
+    {
+      text: "**This emphasis has more than one sentence. It has two lines.**",
+      output:
+        "**This emphasis has more than one sentence.\nIt has two lines.**",
+      errors: [
+        {
+          message: "More than one sentence per line"
         }
       ]
     }
-    // multiple match
-    //         {
-    //             text: `It has many bugs.
-
-    // One more bugs`,
-    //             errors: [
-    //                 {
-    //                     message: "Found bugs.",
-    //                     line: 1,
-    //                     column: 13
-    //                 },
-    //                 {
-    //                     message: "Found bugs.",
-    //                     line: 3,
-    //                     column: 10
-    //                 }
-    //             ]
-    //         },
   ]
 })
